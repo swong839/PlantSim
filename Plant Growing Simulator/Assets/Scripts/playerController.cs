93c86e7744482375f8538941e3693994412812e3
Brian Stone <brianstone@berkeley.edu>
2018-08-29 19:46:04 -0700

    Jet fuel cant melt steel beams

---
+++ Plant Growing Simulator/Assets/Scripts/playerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    
+    Rigidbody rb;
+    
+    public int maxspeed;
+    
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        move();
+	}
+
+    private void move()
+    {
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, 0).normalized * maxspeed;
+        rb.velocity += inputVector - rb.velocity;
+    }
+}
