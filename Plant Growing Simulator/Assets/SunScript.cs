6789d1c653141331bfe4c484043baf37336ef033
Tom Bar Ezer Ayalon <tomtom.barezer@gmail.com>
2018-08-30 16:31:46 -0700

    Sun now moves across the sky and added a line renderer to use as ray

--- Plant Growing Simulator/Assets/SunScript.cs
+++ Plant Growing Simulator/Assets/SunScript.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SunScript : MonoBehaviour {
+public class SunScript : MonoBehaviour
+{
+   [SerializeField]
+   private float m_Speed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   [SerializeField]
+   private float m_MaxHeight;
+
+   private float m_OrigX;
+   private float m_OrigY;
+
+   private float m_Width;
+
+   private bool m_Done;
+
+
+
+   [SerializeField]
+   private LineRenderer m_Ray;
+
+
+
+   private void Awake()
+   {
+      m_OrigX = transform.position.x;
+      m_OrigY = transform.position.y;
+      m_Width = m_MaxHeight / (m_OrigX * m_OrigX);
+   }
+   
+
+
+   private void Update()
+   {
+      if (!m_Done)
+         transform.position = UpdatePos(transform.position.x + m_Speed * Time.deltaTime);
+      if (transform.position.x > -m_OrigX)
+         m_Done = true;
+   }
+
+   private Vector2 UpdatePos(float x)
+   {
+      float newY = - m_Width * x * x + m_MaxHeight + m_OrigY;
+
+      return new Vector2(x, newY);
+   }
 }
