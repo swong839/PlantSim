06c72f05975001f9028f39c4d334048814753be7
unknown <sabrina.wong@berkeley.edu>
2018-08-31 01:42:26 -0700

    menu and audio added

---
+++ Plant Growing Simulator/Assets/Scripts/sceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneController : MonoBehaviour {
+
+    string selfScene;
+
+	// Use this for initialization
+	void Start () {
+        selfScene = SceneManager.GetActiveScene().name;
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(selfScene);
+        }
+    }
+
+    public void GoToScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ Plant Growing Simulator/Assets/Scripts/soundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour {
+
+    private static bool exists = false;
+
+    private void Awake()
+    {
+        if (!exists)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            exists = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
